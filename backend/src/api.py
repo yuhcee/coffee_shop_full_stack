a05a9f0b052477894d2bbd4568a0877138037295
yuhcee <egbouchenna001@gmail.com>
2022-07-24 17:30:46 +0100

    chore(api):add POST /drinks endpoint

--- backend/src/api.py
+++ backend/src/api.py
@@ -47,22 +47,29 @@ def get_drinks_detail(payload):
     return jsonify({"success": True, "drinks": [drink.long() for drink in drinks]}), 200
 
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
-
-
 @ app.route('/drinks', methods=["POST"])
 @ requires_auth("post:drinks")
 def create_drink(payload):
-    # print("===>>", payload)
-    return "Drinks successfully added."
+    body = request.get_json()
+
+    try:
+        title = body.get("title", None)
+        recipe = body.get("recipe", None)
+
+        if type(recipe) is dict:
+            recipe = [recipe]
+
+        drink = Drink(
+            title=title,
+            recipe=json.dumps(recipe)
+        )
+
+        drink.insert()
+
+    except:
+        abort(400)
+
+    return jsonify({'success': True, 'drinks': [drink.long()]})
 
 
 '''
